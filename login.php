bba61c0412844a5303859baacb986d4420bdc3b7
chenjunCodingGit <271676725@qq.com>
2017-05-12 01:15:48 +0800

    解决跨域和跨域session

--- login.php
+++ login.php
@@ -1,10 +1,17 @@
-<?php 
-// session_start();
+<?php
+// header('P3P: CP="CURa ADMa DEVa PSAo PSDo OUR BUS UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR"');
+// ini_set('session.name', 'sid'); //设置session_id的键名
+// ini_set('session.use_trans_sid', 0); //不使用 GET/POST 变量方式
+ini_set('session.use_cookies', 1); //使用cookie保存session_id的方式
+ini_set('session.cookie_path', '/');
+ini_set('session.cookie_domain', '192.168.155.2');
+session_start();
 
 header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Methods:GET');
-header('Access-Control-Allow-Methods:POST');
-header('Access-Control-Allow-Headers:x-requested-with,contenttype');
+// header('Access-Control-Allow-Methods:GET');
+// header('Access-Control-Allow-Methods:POST');
+// header('Access-Control-Allow-Headers:x-requested-with,contenttype');
+
 
 $mysqliConn=new mysqli();
 
@@ -15,8 +22,9 @@ $mysqliConn->connect('localhost', 'root', '', 'mydb');
         exit();
     }
 //数据库查询所有（比较）
-$name=$_POST['name']; 
-$password=$_POST['password'];
+$name=$_GET['name']; 
+$password=$_GET['password'];
+$callback = $_GET['callback'];
 
 $arr = array();	
 //$username = $_POST['username'];
@@ -29,19 +37,21 @@ $row=mysqli_fetch_array($result);
 // echo $_SESSION['user'];
 
 if(mysqli_num_rows($result) == 1){
-	// $_SESSION['user'] = $row[0];
+	$_SESSION['user'] = $row[0];
 	$ret = array(
         'status' => 0, // 0成功，1失败
         'msg'   => 'success'
     );
-    echo json_encode($ret);
+    echo $callback.'('.json_encode($ret).')';
+    // echo json_encode($ret);
     exit();
 }else{
 	$ret = array(
         'status' => 1, // 0成功，1失败
         'msg'   => 'failed'
     );
-    echo json_encode($ret);
+    echo $callback.'('.json_encode($ret).')';
+    // echo json_encode($ret);
     exit();
 }
 ?>
--- session.php
+++ session.php
@@ -1,21 +1,32 @@
 <?php
+// header('P3P: CP="CURa ADMa DEVa PSAo PSDo OUR BUS UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR"');
+// ini_set('session.name', 'sid'); //设置session_id的键名
+// ini_set('session.use_trans_sid', 0); //不使用 GET/POST 变量方式
+ini_set('session.use_cookies', 1); //使用cookie保存session_id的方式
+ini_set('session.cookie_path', '/');
+ini_set('session.cookie_domain', '192.168.155.2');
 session_start();
 header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Methods:GET');
-header('Access-Control-Allow-Methods:POST');
-header('Access-Control-Allow-Headers:x-requested-with,contenttype');
+// header('Access-Control-Allow-Methods:GET');
+// header('Access-Control-Allow-Methods:POST');
+// header('Access-Control-Allow-Headers:x-requested-with,contenttype');
+
+$callback = $_GET['callback'];
+
 if (!empty($_SESSION['user'])) {
-     echo $_SESSION['user'];
-    // $ret = array(
-    //     'status' => $_SESSION['user']
-    // );
+    $ret = array(
+        'status' => $_SESSION['user']
+    );
     // echo json_encode($ret);
-    //exit();}
+    echo $callback.'('.json_encode($ret).')';
+    exit();
+ }
 else{
 	$ret = array(
         'status' => 0
     );
-    echo json_encode($ret);
+    echo $callback.'('.json_encode($ret).')';
+    // echo json_encode($ret);
 	exit();
 }
 ?>
